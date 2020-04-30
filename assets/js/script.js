64d41ad2ff1cf17c24b447235e051d73e0c9000f
PierreQ <pierrequintero@gmail.com>
2020-04-30 01:16:54 -0400

    smooth scroll added and a footer behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+
+
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      800,
+      'linear'
+    )
+  })
